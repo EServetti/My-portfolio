ed64a96e871d0ea27ec9467415641fa49473d347
unknown <eservetti2018@gmail.com>
2024-11-01 16:41:21 -0300

    Mejorada home y contact

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,25 +3,28 @@ import "../styles/home.css";
 import nodeLogo from "../assets/node-logo.png";
 import gitLogo from "../assets/git-hub.png";
 import reactLogo from "../assets/react.svg";
-import { Link } from "react-router-dom";
+import mongoLogo from "../assets/mongo-logo.png";
+import personalPhoto from "../assets/personal-image.jpg"
 
 const Home = () => {
   return (
     <div className="home-container">
       <section className="hero">
         <h1>Welcome to My Portfolio</h1>
-        <p>
-          Hello, I'm Emilio, I am a programming student with courses in backend
-          development using Node.js and Express, and some experience in React.
-          Explore my work and get in touch!
-        </p>
-        <p>
-          To know more about me click <Link to="/about">here</Link>
-        </p>
+        <span>
+          <img src={personalPhoto} alt="me" className="personal-photo" />
+          <p>
+            Hello, I'm Emilio, I am a programming student with courses in
+            backend development using Node.js and Express, and some experience
+            in React, I also can manage NoSQL databases like Mongo DB. Explore
+            my work and get in touch!
+          </p>
+        </span>
         <div className="tech-logos">
           <img src={nodeLogo} alt="Node.js" className="tech-logo" />
           <img src={gitLogo} alt="Git Hub" className="tech-logo" />
           <img src={reactLogo} alt="React" className="tech-logo" />
+          <img src={mongoLogo} alt="Mongo" className="tech-logo" />
         </div>
       </section>
     </div>
